8c4d822e6ec310155132d9253762a9b6315caa21
Jonas Höchst <git@jonashoechst.de>
2024-07-22 20:53:35 +0200

    fixed ug2tex for python3

--- Tools/ug2tex.py
+++ Tools/ug2tex.py
@@ -1,63 +1,67 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import sys, re
+import re
+import sys
 
-legit_chords = ["A","B","H","C","C#","D","D#","E","F","F#","G","G#"]
-legit_chords += [chord+"m" for chord in legit_chords]
-legit_chords += [chord+"7" for chord in legit_chords]
-legit_chords += [chord+"sus2" for chord in legit_chords]
-legit_chords += [chord+"sus4" for chord in legit_chords]
+legit_chords = ["A", "B", "H", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+legit_chords += [chord + "m" for chord in legit_chords]
+legit_chords += [chord + "7" for chord in legit_chords]
+legit_chords += [chord + "sus2" for chord in legit_chords]
+legit_chords += [chord + "sus4" for chord in legit_chords]
 legit_chords += ["Cadd9"]
+
 
 def isChordLine(line):
     # criteria: more than half of the words are legit chords
     chord_count = sum([c in legit_chords for c in line.split()])
     # sys.stderr.write("{} / {} chords are legit: {}\n".format(chord_count, len(line.split()), line))
-    if chord_count > 0 and float(chord_count) / len(line.split()) > 0.5: 
+    if chord_count > 0 and float(chord_count) / len(line.split()) > 0.5:
         return True
 
     return False
-    
+
+
 def getChordLocations(line):
     chords = []
-    for m in re.finditer(r'\S+', line):
+    for m in re.finditer(r"\S+", line):
         index, chord = m.start(), m.group()
         chords.append((index, chord))
     return chords
 
+
 def mergeChordsAndLine(chords, line):
     positions = [0] + [chord[0] for chord in chords] + [len(line)]
-    inserts = ["\["+chord[1]+"]" for chord in chords]
+    inserts = ["\\[" + chord[1] + "]" for chord in chords]
     parts = [line[i:j] for i, j in zip(positions[:-1], positions[1:])]
     merged = [item for pair in zip(parts, inserts) for item in pair] + [parts[-1]]
 
     return "".join(merged)
 
+
 def cleanupLine(line):
     return line.replace("´", "'")
-    
+
 
 if __name__ == "__main__":
-    
-    sys.stdout.write('''%!TEX root = ../Single-Song.tex
+    sys.stdout.write("""%!TEX root = ../Single-Song.tex
 \\beginsong{Song}[]
 
 \\beginverse
-''')
-    
+""")
+
     for line in sys.stdin:
         line = cleanupLine(line)
         if isChordLine(line):
             chords = getChordLocations(line)
-            lyrics = cleanupLine(sys.stdin.next())
-            
+            lyrics = cleanupLine(next(sys.stdin))
+
             chordline = mergeChordsAndLine(chords, lyrics)
             sys.stdout.write(chordline)
         else:
             sys.stdout.write(line)
             pass
-    
-    sys.stdout.write('''
+
+    sys.stdout.write("""
 \\endverse
 
 \\endsong
@@ -66,4 +70,4 @@ if __name__ == "__main__":
 
 \\begin{intersong}
 
-\\end{intersong}''')
+\\end{intersong}""")
